ebff2bdf719be8d15090c621f4da00064ba60260
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-10 21:52:36 +0600

    data added

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,10 +1,5 @@
 import React from 'react';
 import './Services.css';
-import repair1 from '../../../images/repair1.png';
-
-const servies = [
-    { id: 1, name: 'oil change', price: 100, description: '', img: repair1 }
-]
 
 const Services = () => {
     return (
