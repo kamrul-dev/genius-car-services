b1a37ae2101e6259dd8bec189bc969a96ef39efe
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-11 00:20:15 +0600

    Build Responsive React website using Bootstrap

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Experts from '../Experts/Expert';
 import Services from '../Services/Services';
 
 const Home = () => {
     return (
         <div>
             <Services></Services>
+            <Experts></Experts>
         </div>
     );
 };
