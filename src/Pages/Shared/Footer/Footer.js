83097a98d2b23588dd77f26632f85eb3ca893012
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-10 20:58:40 +0600

    Component Structured

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p><small>&c; Copyright 2022</small></p>
+        </footer>
+    );
+};
+
+export default Footer;
