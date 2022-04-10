ff3ba624918e235c2f96281e0716194bc0fd776b
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-10 22:26:31 +0600

    Make Services Responsive using CSS media query

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -12,13 +12,15 @@ const Services = () => {
     }, []);
     return (
         <div>
-            <h3>servies: {services.length}</h3>
-            {
-                services.map(service => <Service
-                    key={service.id}
-                    service={service}
-                ></Service>)
-            }
+            <h1 className='services-title'>Our Services</h1>
+            <div className='services-container'>
+                {
+                    services.map(service => <Service
+                        key={service.id}
+                        service={service}
+                    ></Service>)
+                }
+            </div>
         </div>
     );
 };
