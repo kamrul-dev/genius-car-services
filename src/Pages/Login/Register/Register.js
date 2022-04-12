5d7c4917a81431be2465ce75f9e07d6a597d497c
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-12 23:46:14 +0600

    created register form and style it

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './Register.css';
 
 const Register = () => {
+    const navigate = useNavigate();
+
+    const navigateLogin = () => {
+        navigate('/login');
+    }
+
+    const handleRegister = event => {
+        event.preventDefault();
+    }
     return (
-        <div>
-            <h2>Please Register</h2>
+        <div className='register-form'>
+            <h2 className='text-primary' style={{ textAlign: 'center' }}>Please Register</h2>
+            <form onSubmit={handleRegister}>
+                <input type="text" name="name" id="" placeholder='Your Name' />
+                <input type="email" name="email" id="" placeholder='Your Email' required />
+                <input type="password" name="password" id="" placeholder='Your Passord' required />
+                <input type="submit" value="Register" />
+            </form>
+            <p>Already have an account ? <Link to='/login' className='text-danger pe-atuo text-decoration-none' onClick={navigateLogin}>Please Login</Link> </p>
         </div>
     );
 };
