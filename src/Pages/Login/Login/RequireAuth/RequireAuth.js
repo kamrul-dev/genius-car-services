6c569564f48ef0d7f0ea364e3aba865602746da7
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-19 02:15:18 +0600

    Do not allow protected route without email verification

--- src/Pages/Login/Login/RequireAuth/RequireAuth.js
+++ src/Pages/Login/Login/RequireAuth/RequireAuth.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
+import { useAuthState, useSendEmailVerification } from 'react-firebase-hooks/auth';
 import { Navigate, useLocation } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
 import auth from '../../../../firebase.init';
 import Loading from '../../../Shared/Loading/Loading';
 
@@ -8,13 +9,32 @@ import Loading from '../../../Shared/Loading/Loading';
 const RequireAuth = ({ children }) => {
     const [user, loading] = useAuthState(auth);
     const location = useLocation();
+    const [sendEmailVerification, sending, error] = useSendEmailVerification(auth);
 
-    if(loading){
+    if (loading) {
         return <Loading></Loading>
     }
 
     if (!user) {
         return <Navigate to="/login" state={{ from: location }} replace />;
+    }
+    if (!user.emailVerified) {
+        return <div className='d-flex justify-content-center mt-5'>
+            <div>
+                <h3 className='text-danger'>Your Email is not verified</h3>
+                <h5 className='text-success'>Please verify your email address</h5>
+                <button
+                    className='btn btn-primary'
+                    onClick={async () => {
+                        await sendEmailVerification();
+                        toast('Sent email');
+                    }}
+                >
+                    Resend Verification Email
+                </button>
+            </div>
+            <ToastContainer></ToastContainer>
+        </div>
     }
     return children;
 };
