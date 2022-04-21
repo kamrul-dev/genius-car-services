d27244f481464e21c2bfc2cecae4a2cf4187d205
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-21 21:53:49 +0600

    social login authentication fixed

--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -4,25 +4,27 @@ import fb from '../../../images/social/fb-icon.png';
 import github from '../../../images/social/git-icon.png';
 import { useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import Loading from '../../Shared/Loading/Loading';
 const SocialLogin = () => {
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
     const [signInWithGithub, user1, loading1, error1] = useSignInWithGithub(auth);
     const navigate = useNavigate();
+    const location = useLocation();
+
+    const from = location.state?.from?.pathname || "/";
 
     let errorElement;
-
-    if(loading || loading1){
-        return <Loading></Loading>
-    }
-    
     if (error || error1) {
-        errorElement =<p className='text-danger'>Error: {error?.message} {error1?.message}</p>
+        errorElement = <p className='text-danger'>Error: {error?.message}</p>
+    }
+
+    if (loading || loading1) {
+        return <Loading></Loading>
     }
 
     if (user || user1) {
-        navigate('/home');
+        navigate(from, { replace: true });
     }
 
     return (
